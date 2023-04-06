cf420011d79d9d6d85cdd5d856dd78fe07c9d91d
Momin2004 <m.musa@student.avans.nl>
2023-04-06 15:01:27 +0200

    fixed gradle

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,18 +3,31 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        val lastAccessPoint = PHAccessPoint()
+//        lastAccessPoint.setIpAddress("192.168.1.1:8000") // Enter the IP Address and Port your Emulator is running on here.
+//
+//        lastAccessPoint.setUsername("newdeveloper") // newdeveloper is loaded by the emulator and set on the WhiteList.
+//
+//        phHueSDK.connect(lastAccessPoint)
+
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +35,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Scaffold { innerpadding ->
+                        ConnectButton()
+                        SetButton()
+                        DataCard()
+                    }
                 }
             }
         }
@@ -40,4 +57,60 @@ fun DefaultPreview() {
     MyApplicationTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun ConnectButton(){
+    Row(
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Button(
+            onClick = {
+                //connect
+            },
+            modifier = Modifier
+                .padding(top = 20.dp, start = 30.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.onPrimary
+            )
+
+        ) {
+
+        }
+
+    }
+}
+
+@Composable
+fun SetButton(){
+    Row(
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Button(
+            onClick = {
+                //send data
+            },
+            modifier = Modifier
+                .padding(top = 20.dp, start = 30.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.onPrimary
+            )
+
+        ) {
+
+        }
+
+    }
+}
+
+@Composable
+fun DataCard(){
+    LazyColumn {
+
+    }
+
 }
