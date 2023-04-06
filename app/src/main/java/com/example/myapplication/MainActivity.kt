9b5790f24a2718eaed71fcdc499f0a2e9a73f0fc
Coen <cc.denboer1@student.avans.nl>
2023-04-06 17:01:26 +0200

    added general request method

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,20 +15,16 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import com.philips.lighting.hue.sdk.PHAccessPoint
-import com.philips.lighting.hue.sdk.PHHueSDK
-import com.philips.lighting.hue.sdk.PHSDKListener
 import com.philips.lighting.hue.sdk.*
 import com.philips.lighting.hue.sdk.exception.*
-import com.philips.lighting.hue.sdk.upnp.PHBridgeSearchManagerImpl
 import com.philips.lighting.hue.sdk.utilities.*
 import com.philips.lighting.model.PHBridge
 import com.philips.lighting.model.PHHueParsingError
-import com.philips.lighting.model.PHLightState
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
 import java.net.URL
@@ -36,6 +32,8 @@ import java.net.URL
 
 class MainActivity : ComponentActivity() {
     val TAG = "MainActivity"
+    var bridge = ""
+    val ip = "10.0.2.2:100/api/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //lastAccessPoint.setIpAddress("10.0.2.2:800") // Enter the IP Address and Port your Emulator is running on here.
@@ -57,41 +55,121 @@ class MainActivity : ComponentActivity() {
             }
         }
 
+
+        makeBridgeConnection()
+
+        Log.d(TAG, "waiting for bridge")
+
+        while (bridge.equals(""))
+            sleep(10)
+
+        makeBridgeConnection()
+
+        Log.d(TAG, "go bridge")
+        makeRequest(ip, "/lights/1/state", "PUT", "{\"on\":false}")
+
+    }
+
+    private fun makeBridgeConnection() {
         val username = "coen1" // Replace with your Hue bridge username
         val bridgeIp = "10.0.2.2:100/api" // Replace with your Hue bridge IP address
         val lightId = "1" // Replace with the ID of the light you want to control
         val message = "{\"devicetype\":\"my_hue_app#coen1\"}"
         val method = "GET"
-        HueTask().execute(bridgeIp, username, lightId, message, method)
-
+        HueBridgeTask().execute(bridgeIp, username, lightId, message, method)
+        Log.d(TAG, "Making bridge")
 
     }
-    inner class HueTask : AsyncTask<String, Void, String>() {
+
+    private fun makeRequest(adress: String, command: String, method: String, body: String){
+        HueTask().execute(adress, command, method, body)
+    }
+
+    inner class HueBridgeTask : AsyncTask<String, Void, String>() {
 
         override fun doInBackground(vararg params: String?): String {
-            val urlString = "http://" + params[0]
-            Log.d(TAG, urlString)
-            val url = URL(urlString)
-            val connection = url.openConnection() as HttpURLConnection
-            connection.requestMethod = params[4]
-            connection.setRequestProperty("Content-Type", "application/json")
-            connection.doOutput = true
-            val payload = params[3]
-            val writer = OutputStreamWriter(connection.outputStream)
-            writer.write(payload)
-            writer.flush()
-            writer.close()
+            var data =  "error"
+            Log.d(TAG, "Staring new request")
+            while(data.contains("error")){
+                val urlString = "http://" + params[0]
+                Log.d(TAG, urlString)
+                val url = URL(urlString)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = params[4]
+                connection.setRequestProperty("Content-Type", "application/json")
+                connection.doOutput = true
+                val payload = params[3]
+                val writer = OutputStreamWriter(connection.outputStream)
+                writer.write(payload)
+                writer.flush()
+                writer.close()
 
-            val responseCode = connection.responseCode
-            val data = connection.inputStream.bufferedReader().readText()
-            Log.d(TAG, "Response: " + data)
+                val responseCode = connection.responseCode
+                data = connection.inputStream.bufferedReader().readText()
+                Log.d(TAG, "Response: " + data)
+                sleep(100)
+            }
+            val arr = JSONArray(data)
+            val jObj: JSONObject = arr.getJSONObject(0)
+            bridge = jObj.getJSONObject("success").getString("username")
             return data
         }
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            Log.d(TAG, "Response code: $result")
-            //bridgeReceieved(result)
+            Log.d(TAG, "onpostexecute")
+            if (result != null) {
+                Log.d(TAG, result)
+            }
+
+            if (result != null) {
+                if(result.contains("username")){
+                    val arr = JSONArray(result)
+                    val jObj: JSONObject = arr.getJSONObject(0)
+                    bridge = jObj.getJSONObject("success").getString("username")
+                    Log.d(TAG, bridge)
+                }
+            }
+        }
+    }
+    inner class HueTask : AsyncTask<String, Void, String>() {
+
+        override fun doInBackground(vararg params: String?): String {
+            var data =  "error"
+            Log.d(TAG, "starting new general request")
+            while(data.contains("error")){
+                val addres = params[0]
+                val command = params[1]
+                val method = params[2]
+                val body = params[3]
+
+                val urlString = "http://" + addres + bridge + command
+                Log.d(TAG, urlString)
+                val url = URL(urlString)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = method
+                connection.setRequestProperty("Content-Type", "application/json")
+                connection.doOutput = true
+                val payload = body
+                val writer = OutputStreamWriter(connection.outputStream)
+                writer.write(payload)
+                writer.flush()
+                writer.close()
+
+                val responseCode = connection.responseCode
+                data = connection.inputStream.bufferedReader().readText()
+                Log.d(TAG, "Response: " + data)
+                sleep(100)
+            }
+
+            return data
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+
+
         }
     }
 }
